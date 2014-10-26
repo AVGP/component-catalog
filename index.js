1894eb2b67220c4854eded7c1b5831457ef1c605
Martin Naumann <martin@geekonaut.de>
2014-10-26 02:45:30 +0200

    Better path handling

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 var watcher  = require('chokidar').watch(process.argv[2], {
-    ignored: new RegExp("^[^/]+/[^/]+/.+$"),
+    ignored: ignoreFiles,
     persistent: true
   }),
   mustache   = require('mustache'),
   fs         = require('fs'),
   components = [];
 
+function ignoreFiles(path) {
+  var re = new RegExp("^[^/]+/[^/]+/.+$");
+  return !!re.test(path.replace(process.argv[2] + '/', ''));
+}
+
 function renderInventory(components) {
   fs.readFile("index.html.tpl", "utf8", function(err, content) {
     var output = mustache.render(content, {components: components});
-    fs.writeFile("index.html", output, function() { console.log("Written"); });
+    fs.writeFile(( process.argv[3] ? process.argv[3] + '/' : '') + "index.html", output, function() { console.log("Written"); });
   });
 }
 
@@ -18,7 +23,27 @@ console.log("Watching " + process.argv[2]);
 watcher.on('addDir', function(path) {
   if(path === process.argv[2]) return;
   console.log("Added", path);
-  components.push(path.replace(process.argv[2] + '/', ''));
+
+  if(fs.existsSync(path + '/sample')) {
+    console.log("Sample found")
+    var sample = fs.readFileSync(path + '/sample', 'utf8');
+  } else {
+    var sample = '';
+  }
+
+  if(fs.existsSync(path + '/description')) {
+    var desc = fs.readFileSync(path + '/description', 'utf8');
+  } else {
+    var desc = ''
+  }
+
+  var component = {
+    name: path.replace(process.argv[2] + '/', ''),
+    sample: sample,
+    description: desc
+  };
+
+  components.push(component);
   renderInventory(components);
 })
 .on('unlink', function(path) {
